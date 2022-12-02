28ff6808e2619dddb4bfb0496c55bee3c6e831f2
almondkiruthu <almondkiruthu@gmail.com>
2022-12-02 16:32:06 +0300

    Added the first solution

---
+++ Solution.java
@@ -0,0 +1,13 @@
+
+
+public class Solution {
+
+    // CHALLENGE: print It is cool being a programmer
+
+    // SOLUTION
+
+    public static void main(String[] args){
+        System.out.print("It Is Cool being a programmer");
+    }
+    
+}

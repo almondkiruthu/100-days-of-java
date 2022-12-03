3bff071e05f0fdc636eabd9fc6927ce3c1b12ffe
almondkiruthu <almondkiruthu@gmail.com>
2022-12-03 22:37:48 +0300

    Modified the code abit

--- Solution.java
+++ Solution.java
@@ -1,16 +1,19 @@
 
-
 public class Solution {
 
     // CHALLENGE: print It is cool being a programmer
 
     // SOLUTION
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.print("It Is Cool being a programmer");
-    }
-    
 
-    // WELCOME TO DAY  2 LEARN ABOUT VARIABLES
-    // Challenge: Write a program that displays "Ellie is very smart" 5 times. Each time, on a new line.
+        // WELCOME TO DAY 2 LEARN ABOUT VARIABLES
+        // Challenge: Write a program that displays "Ellie is very smart" 5 times. Each time, on a new line.
+
+        System.out.println("Ellie is very smart");
+        System.out.println("Ellie is very smart");
+
+    }
+
 }

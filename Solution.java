ebe26dfc450706a362e64ad48215d32b07674338
almondkiruthu <almondkiruthu@gmail.com>
2022-12-04 19:26:02 +0300

    Just added the comment challenge solution 4

--- Solution.java
+++ Solution.java
@@ -81,6 +81,21 @@ public class Solution {
         System.out.print(" ");
         System.out.println("Year");
 
+
+        // CHALLENGE 4 FOR THE COMMENT SECTION: Comment out some code so that "sum = 12" appears on the screen.
+
+        // SOLUTION:
+        int a = 3;
+        // int a = 10;
+        int b = 6;
+        // int b = 12;
+        // int sum = 1 + a + b;
+        // int sum = 2 + a + b;
+        int sum = 3 + a + b;
+        // int sum = 4 + a + b;
+
+        System.out.println("sum = " + sum);
+
     }
 
 }

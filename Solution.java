d17acb215bfb7fb4404c440b1730274c572ffa5e
almondkiruthu <almondkiruthu@gmail.com>
2022-12-04 19:10:10 +0300

    Leran about comments challenge

--- Solution.java
+++ Solution.java
@@ -32,6 +32,23 @@ public class Solution {
         int age;
         String city;
 
+        // CHALLENGE:Uncomment one line so that the program displays the numbers 12 and 2 (first 12, and then 2).
+         
+
+        // Solution
+
+        int x = 2;
+        int y = 12;
+
+        // y = x * y;
+        y = x + y;
+
+        x = y - x;
+        y = y - x;
+
+        System.out.println(x);
+        System.out.println(y);
+
     }
 
 }

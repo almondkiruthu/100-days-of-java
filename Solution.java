6c0f8e93c872372279410108e17e408659db480f
almondkiruthu <almondkiruthu@gmail.com>
2022-12-06 15:51:34 +0300

    Added the emotional calculator

--- Solution.java
+++ Solution.java
@@ -205,6 +205,40 @@ public class Solution {
 
         System.out.println(seconds);
 
+
+        // INRODUCING: CONTROL FLOW SYSTEMS IN JAVA 
+        // Emotion Detector program 
+
+        Scanner scanner2 = new Scanner(System.in);
+       int emotion = scanner2.nextInt();
+       /*
+       1 - "You are happy!"
+       2 - "You are sad!"
+       3 - "You are angry!"
+       4 - "You are surprised!"
+       other - "Unknown emotion."
+       */
+       
+       // your code goes here
+       switch (emotion){
+           case 1:
+           System.out.println("You are happy!");
+           break;
+           case 2:
+           System.out.println("You are sad!");
+           break;
+           case 3:
+           System.out.println("You are angry!");
+           break;
+           case 4:
+           System.out.println("You are surprised!");
+           break;
+           default :
+           
+           System.out.println("Unknown emotion.");
+       }
+
+
     }
 
 }

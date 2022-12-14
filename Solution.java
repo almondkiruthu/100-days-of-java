361192071d524503e43055efcd88fb85eec8cdc4
almondkiruthu <almondkiruthu@gmail.com>
2022-12-14 10:52:47 +0300

    Added the creating classes and objects section

--- Solution.java
+++ Solution.java
@@ -457,3 +457,26 @@ public class Solution {
 }
 
 }
+// CREATING CLASSES AND LINKING THEM WITH OBJECTS AT THE SAME TIME LINKING THOSE OBJECTS WITH THEIR ATRRIBUTES/METHODS 
+// CHALLENGE: 
+// Create a program to show loading message to your application users.
+// Define a class Loading which has one public method called LoadingMessage(), which should print "Loading" when called.
+// Create an object named loading and call that method. 
+
+// SOLUTION: 
+public class Main {
+    public static void main(String[] args) {
+        
+        //create a Loading object with the same name
+        Loading loading = new Loading();
+        loading.LoadingMessage();
+    }
+ }
+ 
+ class Loading {
+      //complete the class, add LoadingMessage() method
+     public void LoadingMessage(){
+          System.out.println("Loading");
+      }
+      
+ }

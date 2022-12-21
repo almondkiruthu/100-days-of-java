89a46ec49fbc7d20d08743b7385bd86ef8066ff9
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2022-12-21 15:11:15 +0300

    Just added the getter setter program hint

--- Solution.java
+++ Solution.java
@@ -538,3 +538,28 @@ class Customer {
        System.out.println("Room number: " + roomNumber);
    }
 }
+// GETTERS AND SETTERS 
+// Challenging but we can do it 
+
+Check out the code below.
+public class Vehicle {
+    private String color;
+    
+    // Getter
+    public String getColor() {
+        return color;
+    }
+    
+    // Setter
+    public void setColor(String c) {
+        this.color = c;
+    }
+}
+
+class Program {
+    public static void main(String[ ] args) {
+        Vehicle v1 = new Vehicle();
+        v1.setColor("Red");
+        System.out.println(v1.getColor());
+    }
+}

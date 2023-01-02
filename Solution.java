031bce6181cce1f804f1e79ee8683fefa34b32fa
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-01-02 23:16:48 +0300

    Cleaned code

--- Solution.java
+++ Solution.java
@@ -684,6 +684,4 @@ class Ticket {
        return seat;
    }
 }
-// If I were to ask you Almond, what's the reason behind you learning Java. 
-// Today I have seen my mum suffer and wondering where they are going to get money for my school fees 
-// I will work hard to achive my goals and help us gain finances 
+
